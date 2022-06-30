5959aecf69773e1c88903f2b47e2f1900a394b13
LucasLvy <lucaslevy10@gmail.com>
2022-06-30 19:18:36 +0200

    feat(utils): small refacto

--- contracts/utils.py
+++ contracts/utils.py
@@ -8,7 +8,6 @@ from starknet_py.net.client import Client, InvokeFunction
 from starknet_py.net.models import StarknetChainId
 from starkware.python.utils import from_bytes
 from starkware.starknet.public.abi import get_selector_from_name
-from starkware.cairo.common.hash_state import compute_hash_on_elements
 from starkware.starknet.core.os.transaction_hash.transaction_hash import (
     TransactionHashPrefix,
     calculate_transaction_hash_common,
@@ -44,7 +43,7 @@ def mission_statement():
 
 
 async def print_n_wait(client: Client, invocation: InvokeFunction):
-    print("\u001b[35mTransaction Hash: {}\u001b[0m\n".format(invocation.hash))
+    print(f"\u001b[35mTransaction Hash: {invocation.hash}\u001b[0m\n")
 
     try:
         await invocation.wait_for_acceptance()
@@ -56,7 +55,7 @@ async def print_n_wait(client: Client, invocation: InvokeFunction):
     res = await client.get_transaction_receipt(invocation.hash)
 
     if "ACCEPT" in str(res.status):
-        print("\u001b[32;1mTx Results: {}\u001b[0m".format(res.status))
+        print(f"\u001b[32;1mTx Results: {res.status}\u001b[0m")
         for ev in res.events:
             if ev.keys[0] == SUBMIT_TX and len(res.events) == 1:
                 return ev.data
@@ -66,20 +65,18 @@ async def print_n_wait(client: Client, invocation: InvokeFunction):
         print("\u001b[33;1mPayday Results: no payday\n")
 
     else:
-        print("\u001b[31;1mTx Results: {}".format(res.status))
+        print(f"\u001b[31;1mTx Results: {res.status}")
 
     print("\u001b[0m")
-
-    return
 
 
 async def deploy_testnet(
     client, contract_path="", constructor_args=[], additional_data=None
 ):
     data = dict()
-    CONTRACT_ADDRESS = "{}_ADDRESS".format(contract_path.upper())
+    CONTRACT_ADDRESS = f"{contract_path.upper()}_ADDRESS"
     if additional_data:
-        CONTRACT_ADDRESS = "{}_{}".format(CONTRACT_ADDRESS, additional_data)
+        CONTRACT_ADDRESS = f"{CONTRACT_ADDRESS}_{additional_data}"
     if os.getenv("ACCOUNT_CACHE") == "false":
         print("\u001b[35mDisabled local account cache\u001b[0m\n")
     else:
@@ -88,9 +85,7 @@ async def deploy_testnet(
                 data = json.load(json_file)
                 if CONTRACT_ADDRESS in data:
                     print(
-                        "\u001b[35mFound local contract: {}\u001b[0m\n".format(
-                            data[CONTRACT_ADDRESS]
-                        )
+                        f"\u001b[35mFound local contract: {data[CONTRACT_ADDRESS]}\u001b[0m\n"
                     )
 
                     cached = await Contract.from_address(
@@ -99,21 +94,18 @@ async def deploy_testnet(
                     return cached, int(data[CONTRACT_ADDRESS], 16)
 
     os.system(
-        "starknet-compile --account_contract {}.cairo --output {}_compiled.json".format(
-            contract_path, contract_path, contract_path
-        )
+        f"starknet-compile --account_contract {contract_path}.cairo --output {contract_path}_compiled.json"
     )
 
-    compiled = Path("{}_compiled.json".format(contract_path)).read_text()
+    compiled = Path(f"{contract_path}_compiled.json").read_text()
     deployment_result = await Contract.deploy(
         client, compiled_contract=compiled, constructor_args=constructor_args
     )
-    os.system("rm {}_compiled.json".format(contract_path))
+    os.system(f"rm {contract_path}_compiled.json")
 
     if not deployment_result.hash:
         print("\u001b[31mFailed to deploy contract\u001b[0m\n")
         raise ValueError("Failed to deploy contract")
-        return "", ""
 
     print("\u001b[35mDeployment Initialized: ", deployment_result.hash)
     print("\tWaiting for successful deployment...")
@@ -123,7 +115,7 @@ async def deploy_testnet(
     res = await client.get_transaction(deployment_result.hash)
 
     if os.getenv("ACCOUNT_CACHE") != "false":
-        data[CONTRACT_ADDRESS] = "0x{:02x}".format(res.transaction.contract_address)
+        data[CONTRACT_ADDRESS] = f"0x{res.transaction.contract_address:02x}"
         with open(ACCOUNT_FILE, "w") as outfile:
             json.dump(data, outfile, sort_keys=True, indent=4)
         print("\tSuccess - cached in accounts.json")
@@ -151,15 +143,15 @@ async def compile_deploy(client, contract="", args=[], salt=0):
     if hit:
         return cached, cached_addr
 
-    compiled = "{}_compiled.json".format(contract)
-    os.system("starknet-compile {}.cairo --output {}".format(contract, compiled))
+    compiled = f"{contract}_compiled.json"
+    os.system(f"starknet-compile {contract}.cairo --output {compiled}")
     deployment_result = await Contract.deploy(
         client=client,
         compiled_contract=Path(compiled).read_text(),
         constructor_args=args,
         salt=salt,
     )
-    os.system("rm {}".format(compiled))
+    os.system(f"rm {compiled}")
 
     await client.wait_for_tx(deployment_result.hash)
     res = await client.get_transaction(deployment_result.hash)
@@ -170,7 +162,7 @@ async def compile_deploy(client, contract="", args=[], salt=0):
 
 
 async def devnet_funding(data, toAddr):
-    acc_client, acc_addr = devnet_account(data)
+    acc_client, _ = devnet_account(data)
     gas = await Contract.from_address(data["DEVNET_ETH"], acc_client, True)
     await (
         await gas.functions["transfer"].invoke(
